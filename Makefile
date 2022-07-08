1f9b5a1895dcfdec5f31ee2880d19932e838aa1f
v.dimitrijev <vladislav.dimitrijevic@flyeralarm.com>
2022-07-08 10:12:06 +0200

    .

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 dockerize:
 	docker-compose build app
 	docker-compose up -d
+	docker-compose exec app composer update
 	docker-compose exec app composer update laravel/framework
 	docker-compose exec app composer install
 	docker-compose exec app php artisan key:generate
--- docker-compose/mysql/init_db.sql
+++ docker-compose/mysql/init_db.sql
@@ -8,3 +8,12 @@ CREATE TABLE `places` (
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `places` (name, visited) VALUES ('Berlin',0),('Budapest',0),('Cincinnati',1),('Denver',0),('Helsinki',0),('Lisbon',0),('Moscow',1),('Nairobi',0),('Oslo',1),('Rio',0),('Tokyo',0);
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `places` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255),
+  `batch` int(11),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
